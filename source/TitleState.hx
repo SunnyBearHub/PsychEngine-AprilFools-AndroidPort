cfd7795589a88c078dac9260f728ad6911f28aa5
SunnyBearHub <89496388+SunnyBearHub@users.noreply.github.com>
2023-02-11 21:29:08 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -82,6 +82,13 @@ class TitleState extends MusicBeatState
 	public static var updateVersion:String = '';
 
 	override public function create():Void
+	
+	#if android
+        FlxG.android.preventDefaultKeys = [BACK];
+        #end
+		
+	#if android || FlxG.android.justReleased.BACK #end
+	
 	{
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
