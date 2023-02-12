cb055950af9e4a9e2ca491d13bbee564512e3eb0
SunnyBearHub <89496388+SunnyBearHub@users.noreply.github.com>
2023-02-11 21:07:51 -0300

    Update Achievements.hx

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -182,33 +182,3 @@ class AchievementObject extends FlxSpriteGroup {
 		super.destroy();
 	}
 }
-
-//if you want to add the vpad to a state
-#if mobile
-addVirtualPad(LEFT_FULL, A_B);
-#end
-
-//if you want it to have a camera
-#if mobile
-addVirtualPadCamera();
-#end
-
-//if you want to remove it at some moment use
-#if mobile
-removeVirtualPad();
-#end
-
-//if you want to add the hitbox to a state
-#if mobile
-addHitbox();
-#end
-
-//if you want it to have a camera
-#if mobile
-addHitboxCamera();
-#end
-
-//if you want to remove it at some moment use
-#if mobile
-removeHitbox();
-#end
