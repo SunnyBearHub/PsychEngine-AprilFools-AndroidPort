f8506b6b5f560d628f46421f71cac140efe27158
SunnyBearHub <89496388+SunnyBearHub@users.noreply.github.com>
2023-02-11 21:12:23 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -328,3 +328,33 @@ class CreditsState extends MusicBeatState
 		return creditsStuff[num].length <= 1;
 	}
 }
+
+//if you want to add the vpad to a state
+#if mobile
+addVirtualPad(LEFT_FULL, A_B);
+#end
+
+//if you want it to have a camera
+#if mobile
+addVirtualPadCamera();
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeVirtualPad();
+#end
+
+//if you want to add the hitbox to a state
+#if mobile
+addHitbox();
+#end
+
+//if you want it to have a camera
+#if mobile
+addHitboxCamera();
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeHitbox();
+#end
