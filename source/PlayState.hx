72739a61326aa8acae32e6bdc87cea7253f5becb
SunnyBearHub <89496388+SunnyBearHub@users.noreply.github.com>
2023-02-11 21:23:40 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4553,3 +4553,33 @@ class PlayState extends MusicBeatState
 	var curLight:Int = 0;
 	var curLightEvent:Int = 0;
 }
+
+//if you want to add the vpad to a state
+#if mobile
+addVirtualPad(LEFT_FULL, A_B);
+#end
+
+//if you want it to have a camera
+#if mobile
+addVirtualPadCamera();
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeVirtualPad();
+#end
+
+//if you want to add the hitbox to a state
+#if mobile
+addHitbox();
+#end
+
+//if you want it to have a camera
+#if mobile
+addHitboxCamera();
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeHitbox();
+#end
